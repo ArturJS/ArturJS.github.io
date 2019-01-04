8ea40f17a2e32486ea959e93dd2065a81b311697
ArturJS <nizamutdin.art@gmail.com>
2019-01-04 20:55:04 +0400

    Don't generate source maps for production build

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    'gatsby-plugin-no-sourcemaps',
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
