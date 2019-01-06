f4696ed569ef196b89553e563f7243d1d01781a8
ArturJS <nizamutdin.art@gmail.com>
2019-01-06 13:57:35 +0400

    Updates

--- src/pages/test-article.md
+++
@@ -1,7 +0,0 @@
----
-title: test-article
-date: '2018-12-09'
-spoiler: test-article spoiler.
----
-
-TEST
--- src/utils/format-reading-time.js
+++
@@ -1,5 +0,0 @@
-export const formatReadingTime = minutes => {
-    const cups = Math.round(minutes / 5);
-
-    return `${new Array(cups || 1).fill('☕️').join('')} ${minutes} min read`;
-};
